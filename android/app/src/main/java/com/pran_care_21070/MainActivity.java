0a497da4140a4cc4b93b8d9faa1d7457791acfe5
Crowdbotics <team@crowdbotics.com>
2020-10-04 21:11:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pran_care_21070/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pran_care_21070;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "pran_care_21070";
+    }
+}
